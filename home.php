d12e775aae4926249e11a2bd785c889b506edad7
ravik289 <rr5046646@gmail.com>
2024-12-14 23:06:02 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,104 @@
+<html>
+    <head>
+        <title>Home Page</title>
+        <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <label class="s">Surprise Your Loved One</label>
+    <div class="sflex">
+        <div class="s1div">
+            <table>
+                <tr><td><img class="simg" src="regularcake.jfif"></td>
+                
+                <tr><td class="simgtext">Regular Cake</label></td>
+                
+                <tr><td><img class="simg" src="photocake.jfif"></td>
+                
+                <tr><td class="simgtext">Photo Cake</label></td>
+            </table>
+        </div>
+        <div class="s2div">
+            <table>
+                <tr><td><img class="simg" src="gourmetcake.jfif"></td>
+                
+                <tr><td class="simgtext">Gourmet Cake</label></td>
+                
+                <tr><td><img class="simg" src="themecake.jfif"></td>
+                
+                <tr><td class="simgtext">Theme Cake</label></td>
+            </table>
+        </div>
+    </div>
+    <br>
+    <div class="ourback">
+        <br>
+        <label class="oursellertext">Our Best Sellers</label>
+        <div class="ourflex">
+
+            <div class="our1div">
+                <div class="oursimg">
+                    <img src="ours1.jfif" class="ourimage"></img>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Rich Chocolate Truffle Cake</label><br>
+                        $ 549
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours2.jfif" class="ourimage"></img>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Tropical Fruit n Almond Cake</label><br>
+                        $ 699
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours3.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Whipped Cream Pineapple Cake</label><br>
+                        $ 499
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours4.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Kitkat Chocolate Truffle Cake</label><br>
+                        $ 699
+                    </div>
+                </div>
+            </div>
+
+            <div class="our2div">
+                <div class="oursimg">
+                    <img src="ours5.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Chocolate Vanilla Half & Half Cake</label><br>
+                        $ 549
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours6.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Rose Petals N Pistachio Rasmalai <br>Cake</label><br>
+                        $ 699
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours7.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Classic Black Forest Cake</label><br>
+                        $ 549
+                    </div>
+                </div>
+                <div class="oursimg">
+                    <img src="ours8.jfif" class="ourimage"></img><br>
+                    <div class="oursimgtext">
+                        <label class="ourscakename" >Choco Chip Truffle Cake</label><br>
+                        $ 599
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+        <br>
+    </div>
+</body>
+</html>
